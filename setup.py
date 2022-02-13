b80cb8ec2807b30a9bf05dd3668c1b5c785eccb4
Suresh Sonwane <sonwanesuresh95@gmail.com>
2022-02-13 19:46:34 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 import setuptools
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setuptools.setup(
     name="rfm",
-    version="1.0.15",
+    version="1.0.4",
     author="Suresh Sonwane",
     author_email="sonwanesuresh739@gmail.com",
-    description="Package for RFM Analysis and Customer Segmentation",
+    description="Python Package for RFM Analysis and Customer Segmentation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sonwanesuresh95/rfm",
+    project_urls={
+        "Bug Tracker": "https://github.com/sonwanesuresh95/rfm/issues",
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
